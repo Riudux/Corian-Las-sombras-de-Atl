23b493218511e901f9f7de9d8ae9535487cd0a89
MishiroDreams <153705338+MishiroDreams@users.noreply.github.com>
2026-09-10 13:32:42 -0600

    Pausa (solo freeze)
    se creo e incorporo el menu de pausa
    (solo congela la escena)

---
+++ pausa.gd
@@ -0,0 +1,38 @@
+extends CanvasLayer
+
+onready var color_rect = $ColorRect
+onready var vbox = $VBoxContainer
+
+func _ready():
+	# Empezamos con el menú oculto
+	color_rect.visible = false
+	vbox.visible = false
+	
+	# Conectar botones por código
+	$VBoxContainer/btn_reanudar.connect("pressed", self, "reanudar_juego")
+	$VBoxContainer/btn_exit.connect("pressed", self, "_on_btn_exit_pressed")
+	$VBoxContainer/btn_options.connect("pressed", self, "_on_btn_options_pressed")
+
+func _input(event):
+	if event.is_action_pressed("ui_cancel"): # Tecla Escape por defecto
+		if get_tree().paused:
+			reanudar_juego()
+		else:
+			pausar_juego()
+
+func pausar_juego():
+	color_rect.visible = true
+	vbox.visible = true
+	get_tree().paused = true
+
+func reanudar_juego():
+	color_rect.visible = false
+	vbox.visible = false
+	get_tree().paused = false
+
+func _on_btn_exit_pressed():
+	get_tree().paused = false  # ¡Despausar antes de salir!
+	get_tree().quit()
+
+func _on_btn_options_pressed():
+	print("Botón de opciones presionado")
